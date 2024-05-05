916985142594ed4957eac8306c0cd81a89b8ecd1
Giga Uchaneishvili <g.uchaneishvili@tnet.loc>
2024-05-05 14:10:30 +0400

    header is fixed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { MyAutoIcon } from "../ui/icon/Icon";
 export default function Header() {
 	return (
 		<header className="h-20 bg-white w-full">
-			<div className="w-full px-4 lg:mx-auto w-[1050px] flex items-center h-20">
+			<div className="w-full px-4 lg:mx-auto max-w-[1050px] flex items-center h-20">
 				<MyAutoIcon />
 			</div>
 		</header>
